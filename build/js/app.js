590b1de41f8e65e8a540feedc89593138553b899
Sam Wellander <samwellander@gmail.com>
2018-02-15 12:15:15 -0800

    Add bower

--- build/js/app.js
+++ build/js/app.js
@@ -61,13 +61,32 @@ exports.entryModule = Entry;
 var Entry = require('./../js/journal.js').entryModule;
 
 $(document).ready(function() {
+
+
+  //Adds new journal entry title and preview below the form
   $('#journal-form').submit(function(event) {
     event.preventDefault();
     var title = $('#title-input').val();
+
+    //reset title input
+    $('#title-input').val('');
+
     var entry = new Entry(title);
     entry.body = $('#body-input').val();
 
-    $('#entries').append('<h3 class="title-display">' + entry.title + '</h3><p class="body-display">' + entry.getTeaser(entry.body) + '</p><hr><button id="word-count">Count Words</button><button id="vowel-count">Count Vowels</button>');
+    //reset body input
+    $('#body-input').val('');
+
+    $('#entries').append('<div class="entry-unit"><h3 class="title-display">' + entry.title + '</h3><p class="body-display">' + entry.getTeaser(entry.body) + '</p><p class="full-body hidden">' + entry.body + '</p><h2 class="view-more">View More</h2><hr><button class="word-count">Count Words</button><button class="vowel-count">Count Vowels</button></div>');
+  })
+
+  //expand teaser into full entry onclic
+  $('.view-more').click(function() {
+    console.log('clicked');
+    $('p.full-body').removeClass('hidden');
+    $('p.body-display').addClass('hidden');
   })
 })
+
+
 },{"./../js/journal.js":1}]},{},[2]);
